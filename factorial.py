bfcef04f8b54eed17ea7618d19cb9e279894fd5e
neerajkumarDE <neeraj.d.kumar@zs.com>
2024-11-12 22:49:02 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,1 +1,13 @@
 
+number=int(input('Please enter a  number to calculate the factorial of the number'))
+#factorial=1
+if number==1:
+    print('factorial is',number)
+else:
+    for i in range(2,number+1):
+        
+        factorial=factorial*i
+        i=i+1
+    print('factorial is',factorial)
+    
+    
